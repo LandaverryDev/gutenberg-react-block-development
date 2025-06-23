d4ee3e46571735f6675d164dd7e163b05e13fa69
Erick Landaverry <landaverry.dev@gmail.com>
2025-06-23 16:36:26 -0500

    Fix .gitignore to always track plugins.

---
+++ plugins/curvy/build/blocks-manifest.php
@@ -0,0 +1,25 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'curvy' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'create-block/curvy',
+		'version' => '0.1.0',
+		'title' => 'Curvy',
+		'category' => 'widgets',
+		'icon' => 'smiley',
+		'description' => 'Description: Adds a custom Gutenberg block for creating stylish curvy background sections to enhance page layouts.',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'curvy',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'viewScript' => 'file:./view.js'
+	)
+);
